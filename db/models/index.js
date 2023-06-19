f6a7ffa735dde906d4af46fd21e877d442bfdbc8
udaykiranterapalli <udaykiran.t@avyata.com>
2023-06-19 11:15:41 +0530

    debug fix

--- db/models/index.js
+++ db/models/index.js
@@ -8,7 +8,7 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
-const db_debug = require("debug")("XFUNdmate:DB");
+const db_debug = require("debug")(`${process.env.APP_NAME}:DB`);
 
 let sequelize;
 if (config.use_env_variable) {
